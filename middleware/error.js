a864700dae739e505b2b6f98b5ea3ef34f4e30ab
Meelad <Meelad_Dawood@intuit.com>
2020-04-18 15:50:35 -0700

    using the .pre .post ... etc, you can define middleware specifically for your mongoDB operations.
    in our case we wanted to slugift every bootcamp so our client can access it via /this-is-the-slug
    
    using pre we were able to set a middleware that runs prior to a save and it generates a slug and adds it to the slug attribute in the document
    
    key note: DONT USE ES6 arrow function for .this

--- middleware/error.js
+++ middleware/error.js
@@ -2,8 +2,6 @@ const ErrorResponse = require("../utils/errorResponse");
 
 const errorHandler = (err, req, res, next) => {
   let error = { ...err };
-  // Log to console for dev
-  console.log(err);
 
   error.message = err.message;
 
--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const slugify = require("slugify");
 
 const BootcampSchema = new mongoose.Schema({
   name: {
@@ -91,6 +92,12 @@ const BootcampSchema = new mongoose.Schema({
     type: Date,
     default: Date.now,
   },
+});
+
+// Create Bootcamp Slug from name
+BootcampSchema.pre("save", function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
 });
 
 module.exports = mongoose.model("Bootcamps", BootcampSchema);
