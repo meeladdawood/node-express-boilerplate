cf46287902c4e03ffdad8cafbddfa83152e5f7fb
Meelad <Meelad_Dawood@intuit.com>
2020-04-18 14:08:24 -0700

    This an example of how to handle payload validation, the error obj has an array of validation errors so we map through them in line 21 and then send it back to the user
    That way the client can know exactly what is bad in there request

--- middleware/error.js
+++ middleware/error.js
@@ -3,13 +3,23 @@ const ErrorResponse = require("../utils/errorResponse");
 const errorHandler = (err, req, res, next) => {
   let error = { ...err };
   // Log to console for dev
-  console.log(err.stack.red);
+  console.log(err);
 
   error.message = err.message;
 
   if (error.name === "CastError") {
     const message = `Bootcamp not found with id of ${err.value}`;
     error = new ErrorResponse(message, 404);
+  }
+
+  if (error.code === 11000) {
+    const message = "Duplicate field value entered";
+    error = new ErrorResponse(message, 400);
+  }
+
+  if (error.name === "ValidationError") {
+    const message = Object.values(err.errors).map((val) => val.message);
+    error = new ErrorResponse(message, 400);
   }
 
   res.status(error.statusCode || "500").json({
