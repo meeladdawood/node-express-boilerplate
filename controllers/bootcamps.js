f1b69961a0fa832203f28e01e92bfd3da7dbfe65
Meelad <Meelad_Dawood@intuit.com>
2020-04-18 20:28:38 -0700

    got pagination full working, we added a pagination obj that contains next and prev, this makes it easier for the client to request the next and previous page.

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -10,7 +10,7 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
   // Copy req.query
   const reqQuery = { ...req.query };
 
-  const removeFields = ["select", "sort"];
+  const removeFields = ["select", "sort", "limit", "page"];
 
   // Loop over removeFields and delete them from reqQuery
   removeFields.forEach((param) => delete reqQuery[param]);
@@ -39,12 +39,38 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
     query = query.sort("-createdAt");
   }
 
+  // Pageantion
+  const page = parseInt(req.query.page, 10) || 1;
+  const limit = parseInt(req.query.limit) || 100;
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+  const total = await Bootcamp.countDocuments();
+
+  query = query.skip(startIndex).limit(limit);
+
   // Excuting query
   const bootcamps = await query;
+
+  // Pagination result
+  const pagination = {};
+  if (endIndex < total) {
+    pagination.next = {
+      page: page + 1,
+      limit,
+    };
+  }
+
+  if (startIndex > 0) {
+    pagination.prev = {
+      page: page - 1,
+      limit,
+    };
+  }
 
   res.status(200).json({
     success: true,
     count: bootcamps.length,
+    pagination,
     data: bootcamps,
   });
 });
