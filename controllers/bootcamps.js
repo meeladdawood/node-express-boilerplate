7b36f7855c9a2085ab632e07e59ac27c0c17dffa
Meelad <Meelad_Dawood@intuit.com>
2020-04-23 20:04:27 -0700

    added a new endpoint that handles file upload, we have couple checks
    some are for image size, some are for image type
    
    since we are use a static directory for our file hosting, we can just move the file using
    
    file.mv on L201

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -2,6 +2,7 @@ const ErrorResponse = require("../utils/errorResponse");
 const asyncHandler = require("../middleware/async");
 const geocoder = require("../utils/geocoder");
 const Bootcamp = require("../models/Bootcamp");
+const path = require("path");
 
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
   let query;
@@ -160,5 +161,54 @@ exports.getBootcampsInRadius = asyncHandler(async (req, res, next) => {
     success: true,
     count: bootcamps.length,
     data: bootcamps,
+  });
+});
+
+//Upload photo
+// PUT /api/v1/bootcamps/:id/photo
+exports.bootcampPhotoUpload = asyncHandler(async (req, res, next) => {
+  const bootcamp = await Bootcamp.findById(req.params.id);
+
+  if (!bootcamp) {
+    return next(
+      new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+    );
+  }
+
+  if (!req.files) {
+    return next(new ErrorResponse(`Please Upload a file`, 400));
+    s;
+  }
+
+  const file = req.files.file;
+
+  if (!file.mimetype.startsWith("image")) {
+    return next(new ErrorResponse(`Please upload an image file`, 400));
+  }
+
+  if (file.size > process.env.MAX_FILE_SIZE) {
+    return next(
+      new ErrorResponse(
+        `Please upload an image less than ${process.env.MAX_FILE_SIZE}`,
+        400
+      )
+    );
+  }
+
+  // Create Custom file name
+  file.name = `photo_${bootcamp._id}${path.parse(file.name).ext}`;
+
+  file.mv(`${process.env.FILE_UPLOAD_PATH}/${file.name}`, async (err) => {
+    if (err) {
+      console.error(err);
+      return next(new ErrorResponse(`Problem with file upload`, 500));
+    }
+
+    await Bootcamp.findByIdAndUpdate(req.params.id, { photo: file.name });
+
+    res.status(200).json({
+      success: true,
+      data: file.name,
+    });
   });
 });
