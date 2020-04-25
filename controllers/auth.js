1c140212c3ebd1a83a9ec6c5431bd8f33f01f8be
Meelad <Meelad_Dawood@intuit.com>
2020-04-25 16:29:55 -0700

    I added a login endpoint that takes the email and password
    checks wether they are valid or not, then tries to dehash the password from db and checks if they match
    
    if they do match then we return the jwt

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,7 +3,7 @@ const asyncHandler = require("../middleware/async");
 const User = require("../models/User");
 
 //@desc   Register user
-//@route  GET /api/v1/auth/register
+//@route  POST /api/v1/auth/register
 //@access Public
 exports.register = asyncHandler(async (req, res, next) => {
   const { name, email, password, role } = req.body;
@@ -14,6 +14,41 @@ exports.register = asyncHandler(async (req, res, next) => {
     password,
     role,
   });
+
+  //Create a jwt token
+  const token = user.getSignedJwtToken();
+
+  res.status(200).json({
+    success: true,
+    token,
+  });
+});
+
+//@desc   Login user
+//@route  POST /api/v1/auth/login
+//@access Public
+exports.login = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+  // Validate email and password
+  if (!email || !password) {
+    return next(
+      new ErrorResponse("Please provide a valid email and password", 400)
+    );
+  }
+
+  // Check for use
+  const user = await User.findOne({ email }).select("+password");
+
+  if (!user) {
+    return next(new ErrorResponse("Invalid Credentials", 401));
+  }
+
+  // Check if password matches
+  const isMatch = await user.matchPassword(password);
+
+  if (!isMatch) {
+    return next(new ErrorResponse("Invalid Credentials", 401));
+  }
 
   //Create a jwt token
   const token = user.getSignedJwtToken();
--- models/User.js
+++ models/User.js
@@ -45,4 +45,9 @@ UserSchema.methods.getSignedJwtToken = function () {
   });
 };
 
+// Match user entered with hash value in db
+UserSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
 module.exports = mongoose.model("User", UserSchema);
