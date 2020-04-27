db35d4871715f0a8598a5c588d625f6f9bae328d
Meelad <Meelad_Dawood@intuit.com>
2020-04-26 23:33:22 -0700

    added 2 new endpoints, 1 is to update user details
    the other endpoint is to update the logged in users password

--- routes/auth.js
+++ routes/auth.js
@@ -5,6 +5,8 @@ const {
   getMe,
   forgotPassword,
   resetPassword,
+  updateDetails,
+  updatePassword,
 } = require("../controllers/auth");
 const { protect } = require("../middleware/auth");
 
@@ -15,5 +17,7 @@ router.route("/login").post(login);
 router.route("/me").get(protect, getMe);
 router.route("/forgotpassword").post(forgotPassword);
 router.route("/resetpassword/:resettoken").put(resetPassword);
+router.route("/updatedetails").put(protect, updateDetails);
+router.route("/updatepassword").put(protect, updatePassword);
 
 module.exports = router;
